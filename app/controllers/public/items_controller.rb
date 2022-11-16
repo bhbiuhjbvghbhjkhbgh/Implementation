55e0974877f63a73fcac7366684e30684eaafac7
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-11-16 10:36:15 +0000

    Start

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,4 +1,15 @@
 class Public::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
   def show
+     @item = Item.find(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :genre_id, :is_active, :image)
   end
 end
