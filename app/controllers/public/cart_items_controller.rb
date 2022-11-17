070cfea3151f2dff14679fd8d22be3cf79ad5f3d
EC2 Default User <ec2-user@ip-172-31-47-113.ap-northeast-1.compute.internal>
2022-11-17 12:06:52 +0000

    Start

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,12 +1,13 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_items = Cart_Item.all
+    @cart_items = CartItem.all
+    @items = Item.all
   end
   def create
-    binding.pry
-    @cart_item = Cart_Item.new(cart_item_params)
+
+    @cart_item = CartItem.new(cart_item_params)
     @cart_item.save
-    redirect_to cart_items_path
+    redirect_to public_cart_items_path
   end
 
   private
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,8 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_one :orders, through: :order_details
   belongs_to :genre, optional:true
+
+  def with_tax_price
+     (price * 1.1).floor
+  end
 end
